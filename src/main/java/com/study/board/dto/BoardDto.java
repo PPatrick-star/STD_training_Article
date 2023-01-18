8d92fb9bfddf0c4d247575f70aa617253ebd93ee
Tae Young Ryu <yty0819@gmail.com>
2023-01-18 17:58:57 +0900

    Feature#2 - Create read a article API

--- src/main/java/com/study/board/dto/BoardDto.java
+++ src/main/java/com/study/board/dto/BoardDto.java
@@ -3,13 +3,14 @@ package com.study.board.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 public class BoardDto {
 
     private Long boardPk;
-    private LocalDateTime postDate;
+    private String boardTitle;
+    private String boardContent;
+    private String boardWriter;
+    private String postDate;
 
 }
--- src/main/java/com/study/board/mapper/BoardMapper.java
+++ src/main/java/com/study/board/mapper/BoardMapper.java
@@ -7,4 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface BoardMapper {
 
     int insertOne(BoardVo boardVo);
+
+    BoardVo getReadBoard(Long boardPk);
 }
